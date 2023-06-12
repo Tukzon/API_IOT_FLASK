cd6bd90e7ea0cd1bd1c6360b4fd9befb7122e2b3
Gonzalo Salazar Muñoz <gonsalazarm@gmail.com>
2023-06-11 21:36:57 -0400

    mejoras a sensor_data

--- controllers/sensor_data.py
+++ controllers/sensor_data.py
@@ -4,21 +4,35 @@ import time
 
 ct_sensor_data = Blueprint('ct_sensor_data', __name__)
 
-def get_data(sensor_api_key, from_timestamp, to_timestamp):
+def get_data(sensor_api_key, from_timestamp, to_timestamp, sensor_id):
     db = get_db_connection()
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM sensor_data, sensor WHERE sensor_data.sensor_id = sensor.sensor_id AND sensor.sensor_api_key = %s AND sensor_data.sensor_data_timestamp BETWEEN %s AND %s', (sensor_api_key, from_timestamp, to_timestamp))
-    data = cursor.fetchall()
+    cursor.execute('SELECT * FROM sensor WHERE sensor_api_key = %s', (sensor_api_key,))
+    sensor = cursor.fetchone()
+    data = []
+    if sensor is None:
+        return jsonify({"message": "Sensor not found", "status": 400})
+    if len(sensor_id) > 0:
+        for sensor in sensor_id:
+            cursor.execute('SELECT * FROM sensor_data, sensor WHERE sensor_data.sensor_id = sensor.sensor_id AND sensor.sensor_api_key = %s AND sensor_data.sensor_data_timestamp BETWEEN %s AND %s AND sensor_data.sensor_id IN %s', (sensor_api_key, from_timestamp, to_timestamp, sensor))
+            data += cursor.fetchall()
+    else:
+        cursor.execute('SELECT * FROM sensor_data, sensor WHERE sensor_data.sensor_id = sensor.sensor_id AND sensor.sensor_api_key = %s AND sensor_data.sensor_data_timestamp BETWEEN %s AND %s', (sensor_api_key, from_timestamp, to_timestamp))
+        data = cursor.fetchall()
     db.close()
     return jsonify(data, 200)
 
 
 def create_data():
     db = get_db_connection()
-    current_time = time.time()
     sensor_data = request.get_json()
     cursor = db.cursor()
-    for variable in sensor_data['sensor_data']:
+    cursor.execute('SELECT * FROM sensor WHERE sensor_api_key = %s', (sensor_data['api_key'],))
+    sensor = cursor.fetchone()
+    if sensor is None:
+        return jsonify({"message": "Sensor not found", "status": 400})
+    for variable in sensor_data['json_data']:
+        current_time = time.time()
         cursor.execute('INSERT INTO sensor_data (sensor_data_variable, sensor_data_value, sensor_data_timestamp, sensor_id) VALUES (%s, %s, %s, %s)', (variable['nombre'], variable['valor'], current_time, sensor_data['sensor_id']))
     db.commit()
     db.close()
